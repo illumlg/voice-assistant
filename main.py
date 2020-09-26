c04f9c6671ff7a0c41ba731e63d6be257f65f431
Artem <artem@gmail.com>
2020-09-26 14:59:53 +0300

    Create an initial project structure. fixes #1

---
+++ main.py
@@ -0,0 +1,32 @@
+def run_greeting():
+    pass
+
+def shutdown():
+    pass
+
+def open_website(name):
+    pass
+
+def get_weather(city):
+    pass
+
+def get_localtime():
+    pass
+
+def get_news(limit):
+    pass
+
+def search_topic(topic):
+    pass
+
+def get_joke():
+    pass
+
+def listen():
+    pass
+
+def say(text):
+    pass
+
+def assistant(text_speech):
+    pass
