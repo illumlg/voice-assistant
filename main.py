70bee2a871b9b1f6cd8ad2cc01d0a5a8d53c2561
stevi <takomassoti@gmail.com>
2020-09-29 08:27:34 +0300

    Added the ability to announce the weather report. Fixes #5

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pyttsx3
 import speech_recognition as sr
+from pyowm import OWM
 
 def run_greeting():
     pass
@@ -11,7 +12,22 @@ def open_website(name):
     pass
 
 def get_weather(city):
-    pass
+    owm = OWM('ab0d5e80e8dafb2cb81fa9e82431c1fa')
+    manager = owm.weather_manager()
+    weather = manager.weather_at_place(city).weather
+
+    status = weather.status
+    detailedStatus = weather.detailed_status
+    temperature = weather.temperature('celsius')
+    humidity = weather.humidity
+
+    weather_report = 'Current weather in %s is %s (%s). The temperature is %0.1f degree celcius,' \
+                     ' feels like is %0.1f. The humidity is %s percents' % (
+                         city, status, detailedStatus, temperature['temp'], temperature['feels_like'], humidity)
+
+    print(weather_report)
+    say(weather_report)
+
 
 def get_localtime():
     pass
@@ -26,7 +42,7 @@ def get_joke():
     pass
 
 def listen():
-    speech,r = '', sr.Recognizer()
+    speech, r = '', sr.Recognizer()
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source, duration=2)
         print('Say something...')
